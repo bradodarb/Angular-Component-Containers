018d7f018b2d36c9ef1b9d2c18f40db23735638e
bradodarb <bradodarb@hotmail.com>
2017-07-04 22:22:59 -0700

    exporting models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-/**
- * Export all module and components
- */
-export { ComponentContainerModule } from './component-container.module';
-export { ComponentInjectorService } from './services/component-injector.service';
-export { ContainerItemDirective } from './directives/container-item.directive';
-export { ComponentMap, ComponentMapItem } from './models/component-map';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentInjectorService } from './services/component-injector.service';
+import { ContainerItemDirective } from './directives/container-item.directive';
+import { ComponentMap, ComponentMapItem } from './models/component-map';
+
+export { ComponentMap };
+export { ComponentMapItem };
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ContainerItemDirective],
+  exports: [
+    ContainerItemDirective
+  ],
+  providers: [ComponentInjectorService]
+})
+export class ComponentContainerModule {
+  static defaultMap(map) {
+    ComponentInjectorService.map.init(map);
+  }
+
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: ComponentContainerModule,
+      providers: [ComponentInjectorService]
+    };
+  }
+}
