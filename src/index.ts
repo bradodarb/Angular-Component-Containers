1e43817ac5efba760edb743ecfa647a1f795a277
Brad Murry <bmurry@gmaccm.com>
2017-07-07 10:56:27 -0700

    export ComponentMapService

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './component-container.module';
 export * from './directives/container-item.directive';
 export * from './services/component-injector.service';
+export * from './services/component-map.service';
 export * from './lib/component-map';
